6c17daa1a522af43bb081fb6c8d876e0fffe70e3
BMarcelus <brianmdizon@gmail.com>
2024-06-10 20:58:34 -0700

    Update imageConfig.js

--- src/js/configs/imageConfig.js
+++ src/js/configs/imageConfig.js
@@ -13,8 +13,8 @@ IMAGES.wheelchairBack = ImageLoader.loadImage('wheelchairBack.png');
 IMAGES.wheelchairWheelFront = ImageLoader.loadImage('wheelchairWheelFront.png');
 
 IMAGES.ballotItem = ImageLoader.loadImage('ballotItem.png')
-IMAGES.registrarOffice = ImageLoader.loadImage('registrarOffice.png')
-IMAGES.lowrider = ImageLoader.loadImage('lowrider.png')
+IMAGES.registrarOffice = ImageLoader.loadImage('RegistrarOffice.png')
+IMAGES.lowRider = ImageLoader.loadImage('lowRider.png')
 
 var rgb = hexToRGB;
 function rainbowFunction(data, index, width,height) {
