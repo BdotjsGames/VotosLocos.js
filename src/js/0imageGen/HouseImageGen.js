b3ae2f0aa8a3f3a4d2b894f9493e5f235c42bf0a
bmarcelus <brianmdizon@gmail.com>
2024-06-28 17:47:43 -0700

    Create HouseImageGen.js

---
+++ src/js/0imageGen/HouseImageGen.js
@@ -0,0 +1,13 @@
+function isometricCubeDraw() {
+
+}
+
+function createHouse() {
+    // 18 by 21
+    var canvasElement = document.createElement('canvas');
+    var canvas = canvasElement.getContext('2d');
+    canvasElement.width = 32
+    canvasElement.height = 32;
+
+    return canvasElement
+}
